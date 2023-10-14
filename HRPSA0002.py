f3c1225073e3e64fcad41744149dd6d2b0ae8044
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-14 08:36:12 +0400

    Create HRPSA0002.py

---
+++ HRPSA0002.py
@@ -0,0 +1,18 @@
+#Solution for "Simple Array Sum" https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+#Problem Solving-> Algorithms-> Data Structures
+
+
+def simpleArraySum(ar):
+    sum1=0
+    for i in range(0,len(ar)):
+        sum1+=ar[i]
+    print (sum1)
+
+
+
+n=int(input())
+num=input()
+Arr=list(map(int,num.split()))
+
+simpleArraySum(Arr)
