95aacf863955edcc4ede16a09b256a93971830b4
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-14 13:46:19 +0400

    Create HSPSA0004.py

---
+++ HSPSA0004.py
@@ -0,0 +1,13 @@
+#Solution for "A Very Big Sum" https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+
+#Problem Solving-> Algorithms-> Data Structures
+
+
+def aVeryBigSum(ar):
+    ans=sum(ar)
+    return (ans)
+
+n=int(input())
+a1=input()
+A1=list(map(int,a1.split()))
+print(aVeryBigSum(A1))
