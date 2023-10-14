8e1059875b06f2d2f5639db60480c4ecd95bf414
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-14 13:34:00 +0400

    Create HRPSA0003.py

---
+++ HRPSA0003.py
@@ -0,0 +1,25 @@
+
+def compareTriplets(a, b):
+    c1=0
+    c2=0
+    points=[]
+    for i in range(0,3):
+        if (a[i]>b[i]):
+            c1+=1
+        elif (b[i]>a[i]):
+            c2+=1
+        else:
+            continue
+    points.append(c1)
+    points.append(c2)
+    return (points)         
+   
+a1=input()
+A1=list(map(int,a1.split()))
+b1=input()
+B1=list(map(int,b1.split()))
+
+l=compareTriplets(A1,B1)
+for j in range(0,2):
+    print(l[j], end=" ")
+
