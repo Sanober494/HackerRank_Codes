52556be8aae2275c7ea7f21fc2c2db5276b8e2c6
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-14 08:33:39 +0400

    Create HRPSA0001.py

---
+++ HRPSA0001.py
@@ -0,0 +1,12 @@
+#Solution for "Solve Me First" https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+#Problem Solving-> Algorithms-> Data Structures
+
+def solveMeFirst(a,b):
+	return(a+b)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
