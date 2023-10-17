e71d7636e55fda7dd62878d8c2c7ec58f0b4d4b0
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-17 20:12:49 +0400

    Create HRPSA0007.py

---
+++ HRPSA0007.py
@@ -0,0 +1,23 @@
+#Solution for "Day of the Programmer" https://www.hackerrank.com/challenges/day-of-the-programmer/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+
+def dayOfProgrammer(year):
+    Year=1918
+    if (year==1918):
+        print("26.09.1918")
+    elif (year>Year and (((year%4)==0 and ((year%100)!=0))or ((year%400)==0))):
+        print("12.09.",end="")
+        print(year)
+    elif (year<Year and ((year%4)==0)):
+        print("12.09.",end="")
+        print(year)
+    else:
+        print("13.09.",end="")
+        print(year)
+
+n=int(input())
+dayOfProgrammer(n)
+        
+
