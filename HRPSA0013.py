396283ea82c367a0c7836ba4e74989acc0e4419f
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-25 10:44:32 +0400

    Create HRPSA0013.py

---
+++ HRPSA0013.py
@@ -0,0 +1,17 @@
+
+
+
+def divisibleSumPairs(n, k, ar):
+    c=0
+    for i in range(0,len(ar)):
+        for j in range(i+1,len(ar)):
+            if (((ar[i]+ar[j])%k)==0):
+                c+=1
+    print(c)
+    
+num=input()
+Num=list(map(int,num.split()))
+arr=input()
+Arr=list(map(int,arr.split()))
+divisibleSumPairs(Num[0],Num[1],Arr)
+
