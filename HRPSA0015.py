479a96df260bcefdb3d30b92e0156dee88cc7869
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-10-28 08:12:53 +0400

    Create HRPSA0015.py

---
+++ HRPSA0015.py
@@ -0,0 +1,25 @@
+#Solution for "Counting Valleys" https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+def countingValleys(steps, path):
+    level = 0 
+    valley_count = 0  
+
+    for step in path:
+        if step == "U":
+            level += 1
+        else:
+            level -= 1
+
+    
+        if step == "U" and level == 0:
+            valley_count += 1
+
+    return valley_count
+    
+n1 = int(input())
+stInput = input()
+st = list(stInput)
+result = countingValleys(n1, st)
+print(result)
