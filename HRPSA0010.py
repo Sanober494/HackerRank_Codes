fcfee672d2c3bfb0baeeebca3a0d234a6ccd0f81
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-10 19:19:44 +0400

    Update HRPSA0010.py

--- HRPSA0010.py
+++ HRPSA0010.py
@@ -7,9 +7,7 @@ def kangaroo(x1, v1, x2, v2):
     
     if (x1 < x2 and v1 <= v2) or (x2 < x1 and v2 <= v1):
         print("NO")
-        return
-
-    
+        return    
     if (x1 - x2) % (v2 - v1) == 0:
         print("YES")
     else:
