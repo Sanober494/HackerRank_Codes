5fbfb057d3bbf60c43abbcdf643973aee07a6533
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-12 09:13:05 +0400

    Create HRPSA0016.py

---
+++ HRPSA0016.py
@@ -0,0 +1,21 @@
+#Solution for "Bill Division" 
+#Link: https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+def bonAppetit(bill, k, b):
+    total = sum(bill)
+    anna = (total - bill[k]) // 2
+    
+    if b == anna:
+        print("Bon Appetit")
+    else:
+        print(b - anna)
+
+
+line = input()
+arr = list(map(int, line.split()))
+line1 = input()
+arr1 = list(map(int, line1.split()))
+amtCharged = int(input())
+
+bonAppetit(arr1, arr[1], amtCharged)
