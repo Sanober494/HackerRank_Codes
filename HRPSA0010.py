d1c668229b367d33d04b240b06b52cfdc318bd4e
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-17 09:00:24 +0400

    Update HRPSA0010.py

--- HRPSA0010.py
+++ HRPSA0010.py
@@ -3,8 +3,7 @@
 #Problem Solving-> Algorithms-> Data Structures
 
 
-def kangaroo(x1, v1, x2, v2):
-    
+def kangaroo(x1, v1, x2, v2):    
     if (x1 < x2 and v1 <= v2) or (x2 < x1 and v2 <= v1):
         print("NO")
         return    
