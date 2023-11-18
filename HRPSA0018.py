f266fe7d63374cb7225df4a57357e77f1135c614
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-18 09:27:44 +0400

    Create HRPSA0018.py

---
+++ HRPSA0018.py
@@ -0,0 +1,23 @@
+#Solution for "Beautiful Days at the Movies" 
+#Link: https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+
+
+def beautifulDays(i, j, k):
+    count=0
+    for m in range(i,(j+1)):
+        
+        l=str(m)[::-1]
+        L=int(l)
+        beautyNum=abs(m-L)
+        if beautyNum%k==0:
+            count+=1
+    print(count)
+            
+
+
+numbers=input()
+Num=list(map(int,numbers.split()))
+beautifulDays(Num[0],Num[1],Num[2])
