11f97737ba3e9b566e481d9bfda34c93e709d3b6
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-18 10:02:56 +0400

    Create HRPSA0019.py

---
+++ HRPSA0019.py
@@ -0,0 +1,21 @@
+#Solution for "Designer PDF Viewer" 
+#Link: https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structure
+
+
+def designerPdfViewer(h, word):
+    index=0
+    NewHeight=0
+    for l in word:
+        index=ord(l)-ord("a")
+            
+        if h[index] > NewHeight: 
+            NewHeight=h[index]
+            
+    print(NewHeight*len(word))    
+    
+
+height=input()
+Height=list(map(int,height.split()))
+letter=input()
+designerPdfViewer(Height,letter)
