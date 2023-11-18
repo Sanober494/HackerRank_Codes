8b0a4f82da3364f3669375eb0519227db474122e
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-18 10:15:57 +0400

    Create HRPSA0022.py

---
+++ HRPSA0022.py
@@ -0,0 +1,21 @@
+#Solution for "Mini-Max Sum" 
+#Link: https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structure
+
+def miniMaxSum(arr):
+    min_val = min(arr)
+    max_val = max(arr)
+
+    total_sum = sum(arr)
+
+    min_sum = total_sum - max_val
+    max_sum = total_sum - min_val
+
+    print(min_sum, max_sum)
+
+# Input
+a1 = input()
+n = list(map(int, a1.split()))
+
+# Function call
+miniMaxSum(n)
