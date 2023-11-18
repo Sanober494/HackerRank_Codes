7bf6a263cc45f0e4c864b485d5701d2ba292c4c4
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-18 10:14:43 +0400

    Create HRPSA0021.py

---
+++ HRPSA0021.py
@@ -0,0 +1,15 @@
+#Solution for "Staircase" 
+#Link: https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structure
+
+
+def staircase(n):
+    space= " "
+    hashes="#"    
+    for i in range(1,n+1):
+        print(space*(n-i), end="")
+        print(hashes*i)      
+        
+        
+n1=int(input())
+staircase(n1)
