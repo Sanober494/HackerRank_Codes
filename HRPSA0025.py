cd91b03af9072a3eb05abb3d810dc6b5025891fe
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-23 11:40:30 +0400

    Create HRPSA0025.py

---
+++ HRPSA0025.py
@@ -0,0 +1,31 @@
+#Solution for "Sales by Match" 
+#Link: https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+
+def sockMerchant(n, ar):
+    pairs=0
+    newList=[]
+    for i in range(0,len(ar)):
+        count=0
+        if ar[i] in newList:
+            continue
+        else:
+            for j in range(i+1,len(ar)):
+                if (ar[i]==ar[j]):
+                    count+=1
+            newList.append(ar[i])
+            if (int(count%2)==0):
+                pairs+=int(count/2)
+            else:
+                count+=1
+                pairs+=int(count/2)
+    print(pairs)           
+
+
+
+n1=int(input())
+num=input()
+Num=list(map(int,num.split()))
+sockMerchant(n1,Num)
