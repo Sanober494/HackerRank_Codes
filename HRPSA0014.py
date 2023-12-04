79c791c7e875cd77602e33f45bda26f8edf5fe27
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-04 11:43:07 +0400

    Update HRPSA0014.py

--- HRPSA0014.py
+++ HRPSA0014.py
@@ -6,8 +6,7 @@ from collections import Counter
 def migratoryBirds(arr):  
     birdCounts = Counter(arr)
     maxFrequency = max(birdCounts.values())    
-    maxFrequencyBirds = [birdType for birdType, frequency in birdCounts.items() if frequency == maxFrequency]
-    
+    maxFrequencyBirds = [birdType for birdType, frequency in birdCounts.items() if frequency == maxFrequency] 
     return min(maxFrequencyBirds)
 
 num=int(input())
