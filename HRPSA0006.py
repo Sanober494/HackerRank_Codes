1399765b0149bcba2b07131eae512d6b90f2b44a
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-05 08:00:09 +0400

    Update HRPSA0006.py

--- HRPSA0006.py
+++ HRPSA0006.py
@@ -2,10 +2,7 @@
 #Link: https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
 #Problem Solving-> Algorithms-> Data Structures
 
-
-
 from datetime import datetime
-
 def timeConversion(s):
     t=datetime.strptime(s,"%I:%M:%S%p")
     return (t.strftime("%H:%M:%S"))
