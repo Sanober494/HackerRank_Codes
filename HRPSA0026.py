0a47179ef8a74872cffafbd8e24c3c94adaa519b
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-16 12:39:07 +0400

    Create HRPSA0026.py

---
+++ HRPSA0026.py
@@ -0,0 +1,13 @@
+#Solution for "Drawing Book" 
+#Link: https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+def pageCount(n, p):
+    front = p // 2  
+    back = (n // 2) - (p // 2)  
+    return min(front, back)
+                   
+n= int(input())
+p=int(input())
+print(pageCount(n,p))
