f80552e7f9cd2172bc7446cece6671c4b2b51fb4
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-17 12:28:33 +0400

    Create HRPSA0027.py

---
+++ HRPSA0027.py
@@ -0,0 +1,20 @@
+#Solution for "Picking Numbers"
+#Link: https://www.hackerrank.com/challenges/picking-numbers/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+def pickingNumbers(a):
+    numCount = {}
+    
+    for num in a:
+        numCount[num] = numCount.get(num, 0) + 1
+        numCount[num + 1] = numCount.get(num + 1, 0) + 1
+    
+    maxlen = max(numCount.values())    
+    return maxlen
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = pickingNumbers(arr)
+print(result)
