3a75ae584430ec88ae44f1e09cc0e542eb240483
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-18 11:46:30 +0400

    Create HRPS0028.py

---
+++ HRPS0028.py
@@ -0,0 +1,24 @@
+#Solution for "Angry Professor" 
+#Link: https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+
+
+def angryProfessor(k, a):
+    arr=input()
+    c=0
+    ARR=list(map(int,arr.split()))
+    for i in range(0,len(ARR)):
+        if ARR[i]<=0:
+            c+=1
+    if c>=a:
+        print("NO")
+    else:
+        print("YES")
+    
+t=int(input())
+for i in range (0,t):
+    num=input()
+    NUM=list(map(int,num.split()))
+    angryProfessor(NUM[0],NUM[1])
+    
