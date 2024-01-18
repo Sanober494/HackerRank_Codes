c969e18f6e347d9ec33399e4b5abebfddce48497
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-18 12:14:14 +0400

    Create HRPSA0029.py

---
+++ HRPSA0029.py
@@ -0,0 +1,23 @@
+def missingNumbers(arr, brr):    
+    freq_arr = {}
+    freq_brr = {}    
+    for num in arr:
+        freq_arr[num] = freq_arr.get(num, 0) + 1  
+    for num in brr:
+        freq_brr[num] = freq_brr.get(num, 0) + 1    
+    missing_numbers = []
+    for num in freq_brr:
+        if num not in freq_arr or freq_brr[num] > freq_arr[num]:
+            missing_numbers.append(num)  
+    missing_numbers.sort()
+    print(*missing_numbers)
+
+lenarr = int(input())
+arr_input = input()
+arr = list(map(int, arr_input.split()))
+
+lenarr1 = int(input())
+arr1_input = input()
+arr1 = list(map(int, arr1_input.split()))
+
+missingNumbers(arr, arr1)
