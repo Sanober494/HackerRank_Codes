6dc4eb7d6084f533c57e11a6166087e32c540a36
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-19 11:37:42 +0400

    Create HRPSA0030.py

---
+++ HRPSA0030.py
@@ -0,0 +1,22 @@
+#Solution for "Sherlock and Array" 
+#Link: https://www.hackerrank.com/challenges/sherlock-and-array/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+def balancedSums(arr):
+    total_sum = sum(arr)
+    left_sum = 0
+    
+    for num in arr:
+        total_sum -= num
+        if left_sum == total_sum:
+            return "YES"
+        left_sum += num
+        
+    return "NO"
+
+t = int(input())
+for _ in range(t):
+    num_elements = int(input())
+    num_list = list(map(int, input().split()))
+    result = balancedSums(num_list)
+    print(result)
