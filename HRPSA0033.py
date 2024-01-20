11d5880535585f455305d2c61b73898e97414552
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-20 12:35:45 +0400

    Create HRPSA0033.py

---
+++ HRPSA0033.py
@@ -0,0 +1,14 @@
+#Solution for "Flipping Bits" 
+#Link: https://www.hackerrank.com/challenges/flipping-bits/problem?isFullScreen=true
+#Problem Solving-> Algorithms-> Data Structures
+
+def flippingBits(n):
+    binaryRep = bin(n)[2:].zfill(32)  
+    flippedString = ''.join('1' if bit == '0' else '0' for bit in binaryRep)
+    decimalNum = int(flippedString, 2)  
+    return decimalNum
+
+n1 = int(input())
+for i in range(n1):
+    num = int(input())
+    print(flippingBits(num))
