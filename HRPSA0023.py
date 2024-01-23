785dc46d943be61b38338d4d07f947c9bd829c2a
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-23 15:49:39 +0400

    Update HRPSA0023.py

--- HRPSA0023.py
+++ HRPSA0023.py
@@ -2,7 +2,6 @@
 #Link: https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
 #Problem Solving-> Algorithms-> Data Structure
 
-
 def birthdayCakeCandles(candles):
     max=candles[0]
     count=0
