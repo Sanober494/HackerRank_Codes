4ca2e1335b3b123e530f9672c501f6a93eb0322c
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-01-23 15:50:45 +0400

    Update HRPSA0027.py

--- HRPSA0027.py
+++ HRPSA0027.py
@@ -6,8 +6,7 @@ def pickingNumbers(a):
     numCount = {}    
     for num in a:
         numCount[num] = numCount.get(num, 0) + 1
-        numCount[num + 1] = numCount.get(num + 1, 0) + 1
-    
+        numCount[num + 1] = numCount.get(num + 1, 0) + 1    
     maxlen = max(numCount.values())    
     return maxlen
 
