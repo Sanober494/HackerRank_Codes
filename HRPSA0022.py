4cbe00f0f9ae9d6b5faff56e1286f884d2457ab1
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2024-02-27 20:38:41 +0400

    Update HRPSA0022.py

--- HRPSA0022.py
+++ HRPSA0022.py
@@ -6,6 +6,7 @@ def miniMaxSum(arr):
     min_val = min(arr)
     max_val = max(arr)
     total_sum = sum(arr)
+    
     min_sum = total_sum - max_val
     max_sum = total_sum - min_val
     print(min_sum, max_sum)
